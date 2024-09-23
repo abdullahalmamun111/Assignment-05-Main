7e6f1ff7cd4799db36e0badfaca3435f243895ac
unknown <almamun602767@gmail.com>
2024-09-24 00:28:05 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const historyBtn = document.getElementById('showHistory');
+const donateBtn = document.getElementById('showDonate');
+
+// section show and hide 
+document.getElementById('showHistory').addEventListener('click', function(){
+    sectionHide('history');
+    historyBtn.classList.add('bg-[#B4F461]');
+    historyBtn.classList.add('hover:bg-[#B4F461]')
+    donateBtn.classList.remove('bg-[#B4F461]');
+    donateBtn.classList.remove('hover:bg-[#B4F461]')
+})
+
+document.getElementById('showDonate').addEventListener('click', function(){
+    sectionHide('donate-section');
+    donateBtn.classList.add('bg-[#B4F461]');
+    donateBtn.classList.add('hover:bg-[#B4F461]')
+    historyBtn.classList.remove('bg-[#B4F461]');
+    historyBtn.classList.remove('hover:bg-[#B4F461]')
+})
---
+++ utilities.js
@@ -0,0 +1,71 @@
+function getValueOfId(id){
+    const idName = document.getElementById(id).value;
+    const idParseFloat = parseFloat(idName);
+    return idParseFloat;
+}
+
+
+function idTextValueCapture(id){
+    const capture = document.getElementById(id).innerText;
+    const parseFloatValue = parseFloat(capture);
+    return parseFloatValue ;
+}
+// section hide function
+function sectionHide(id){
+    document.getElementById('donate-section').classList.add('hidden');
+    document.getElementById('history').classList.add('hidden');
+    document.getElementById(id).classList.remove('hidden');
+}
+
+
+
+function Donate(place,amount,totalDonate,mainBalance){
+        // update donate amount
+        const inputAmount = getValueOfId(amount);
+        if(isNaN(inputAmount) || inputAmount < 0){
+            alert('Failed To Donate Plese Type Valid Amount')
+            return ;
+        }
+        const donate = idTextValueCapture(totalDonate);
+        const setDonate = document.getElementById(totalDonate);
+        setDonate.innerText = inputAmount + donate;
+    
+        // update main balance
+        const currentBalance = idTextValueCapture(mainBalance);
+        const setBalance = document.getElementById(mainBalance);
+        setBalance.innerText = currentBalance - inputAmount ;
+        // transaction section added
+        if(place === 'noakhali-Donate'){
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="w-9/12 mx-auto ">
+            <div class="p-[20px] border-solid border-2 rounded-xl mt-8 space-y-2" id="transaction-history">
+            <h1 class = "text-xl ">${inputAmount} Taka Is Doanated For Donate for Flood at Noakhali,Bangladesh</h1>
+        </div>
+    </div>
+    `
+    document.getElementById('history').appendChild(div);
+        }
+    else if(place === 'feni-donate'){
+        const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="w-9/12 mx-auto ">
+            <div class="p-[20px] border-solid border-2 rounded-xl mt-8 space-y-2" id="transaction-history">
+            <h1 class = "text-xl ">${inputAmount} Taka Is Doanated For Donate for Flood at Feni,Bangladesh</h1>
+        </div>
+    </div>
+    `
+    document.getElementById('history').appendChild(div);
+    }
+    else if(place === 'quota-donate'){
+        const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="w-9/12 mx-auto ">
+            <div class="p-[20px] border-solid border-2 rounded-xl mt-8 space-y-2" id="transaction-history">
+            <h1 class = "text-xl ">${inputAmount} Taka Is Doanated For Donate for Quota movement Bangladesh</h1>
+        </div>
+    </div>
+    `
+    document.getElementById('history').appendChild(div);
+    }
+}
